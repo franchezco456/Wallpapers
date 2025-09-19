bc0e6808c5bbd94817ceefd61543618fc0c0f555
franky franco <frankyfranco456@gmail.com>
2025-09-19 14:38:03 -0500

    estructura basica de la pagina login

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Form, FormControl } from '@angular/forms';
+type InputType = 'text' | 'password' | 'email' | 'number';
 
 @Component({
   selector: 'app-input',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class InputComponent  implements OnInit {
-
+  @Input() label : string = '';
+  @Input() placeholder: string = '';
+  @Input() type: InputType = 'text';
+  @Input() control: FormControl = new FormControl();
   constructor() { }
 
   ngOnInit() {}
 
+  public doWrite(event : any){
+    console.log(event.target.value);
+    this.control.setValue(event.target.value);
+  }
 }

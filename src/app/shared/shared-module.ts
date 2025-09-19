10ec4541d7782767f76113af0f1c084f7f27d17e
franky franco <frankyfranco456@gmail.com>
2025-09-19 14:25:30 -0500

    se importa el shared module en la pagina login

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from './components/button/button.component';
+import { CardComponent } from './components/card/card.component';
+import { FbuttonComponent } from './components/Fbutton/fbutton.component';
+import { InputComponent } from './components/input/input.component';
+import { LinkComponent } from './components/link/link.component';
+import { ToggleComponent } from './components/toggle/toggle.component';
+import { IonicModule } from '@ionic/angular';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ButtonComponent,CardComponent,FbuttonComponent, InputComponent, LinkComponent, ToggleComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule, IonicModule
+  ],
+  exports: [ButtonComponent,CardComponent,FbuttonComponent, InputComponent, LinkComponent, ToggleComponent]
 })
 export class SharedModule { }
